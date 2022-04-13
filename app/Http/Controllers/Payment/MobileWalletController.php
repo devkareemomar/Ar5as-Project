3a1ecfd575eb4fe6dcd813ca6d92316b6e613711
almasterhamzawy <alkberhamzay@gmail.com>
2022-04-13 23:43:30 +0200

    complete mobile wallet

--- app/Http/Controllers/Payment/MobileWalletController.php
+++ app/Http/Controllers/Payment/MobileWalletController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Payment;
 
+use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\Controller;
 use App\Order;
 use GuzzleHttp\Client;
@@ -10,17 +11,9 @@ use Illuminate\Support\Str;
 
 class MobileWalletController extends Controller
 {
-    public function createCheckout()
-    {
-        return view("frontend.payment.mobile_wallet");
-    }
-
-    /*=================================================================*/
-    public function requestToPayAction()
+    public function createCheckout($order)
     {
         $referenceKey = Str::random(10);
-
-        $order = Order::findOrFail(request()->session()->get('order_id'));
 
         $date = date('Ymdhis');
 
@@ -28,48 +21,111 @@ class MobileWalletController extends Controller
 
         $total = floatval($total * 100);
 
-        $requestBody = [
-            "AmountTrxn" => "$total",
-            "Currency" => 818,
-            "Validity" => "10",
-            "MerchantReference" => $referenceKey,
-            "MobileNumber" => request()->mobileNumber,
-            "MerchantId" => env("MEEZA_Merchant_ID"),
-            "TerminalId" => env("MEEZA_Terminal_ID"),
-            "BillNumber" => $order->id,
-            "SecureHash" => $this->generateHash($date),
-            "DateTimeLocalTrxn" => $date,
+        $viewConfig = [
+            'orderId' => $referenceKey,
+            'amount' => $total,
+            'meezaCardUrl' => env("MEEZA_URL"),
+            'merchantId' => env("WALLAT_Merchant_ID"),
+            'terminalId' => env("WALLET_Terminal_ID"),
+            'successUrl' => route("meezaCard.success", ['orderId' => $order->id]),
+            'cancelUrl' => route("meezaCard.cancel"),
+            'transactionDate' => $date,
+            'secureHash' => $this->generateHash($date)
         ];
 
-        $data = json_encode($requestBody);
-
-        $client = new Client([
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'Accept' => '*/*',
-                'Accept-Encoding' => 'gzip, deflate',
-            ]
-        ]);
-
-        $endPoint = env("MEEZA_WALLET_URL") . "/RequestToPay";
-
-        $response = $client->post($endPoint,
-            ['body' => $data]
-        );
-
-        $providerResponse = json_decode($response->getBody(), true);
-        dd($providerResponse);
+        return view("frontend.payment.meeza_card", $viewConfig);
     }
 
-    /*=================================================================*/
+    /*==========================================================================*/
     public function generateHash($date)
     {
-        $key = env('MEEZA_Secure_Key');
-        $MEEZA_Merchant_ID = env('MEEZA_Merchant_ID');
-        $MEEZA_Terminal_ID = env('MEEZA_Terminal_ID');
+        $key = env('WALLET_Secure_Key');
+        $MEEZA_Merchant_ID = env('WALLAT_Merchant_ID');
+        $MEEZA_Terminal_ID = env('WALLET_Terminal_ID');
         $secret = hex2bin($key);
         $string = "DateTimeLocalTrxn=$date&MerchantId={$MEEZA_Merchant_ID}&TerminalId={$MEEZA_Terminal_ID}";
         return hash_hmac('sha256', $string, $secret);
     }
+
+    /*==========================================================================*/
+    public function success($orderId)
+    {
+        $payment = ["status" => "Success"];
+
+        $checkoutController = new CheckoutController;
+        return $checkoutController->checkout_done($orderId, json_encode($payment));
+
+    }
+
+    /*==========================================================================*/
+    public function cancel()
+    {
+        flash(translate('Payment is cancelled'))->error();
+        return redirect()->route('home');
+    }
+    /*==========================================================================*/
+
+
+//    public function createCheckout()
+//    {
+//        return view("frontend.payment.mobile_wallet");
+//    }
+
+    /*=================================================================*/
+//    public function requestToPayAction()
+//    {
+//        $referenceKey = Str::random(10);
+//
+//        $order = Order::findOrFail(request()->session()->get('order_id'));
+//
+//        $date = date('Ymdhis');
+//
+//        $total = number_format(floatval($order->grand_total), 2, '.', '');
+//
+//        $total = floatval($total * 100);
+//
+//        $requestBody = [
+//            "AmountTrxn" => "$total",
+//            "Currency" => 818,
+//            "Validity" => "10",
+//            "MerchantReference" => $referenceKey,
+//            "MobileNumber" => request()->mobileNumber,
+//            "MerchantId" => env("MEEZA_Merchant_ID"),
+//            "TerminalId" => env("MEEZA_Terminal_ID"),
+//            "BillNumber" => $order->id,
+//            "SecureHash" => $this->generateHash($date),
+//            "DateTimeLocalTrxn" => $date,
+//        ];
+//
+//        $data = json_encode($requestBody);
+//
+//        $client = new Client([
+//            'headers' => [
+//                'Content-Type' => 'application/json',
+//                'Accept' => '*/*',
+//                'Accept-Encoding' => 'gzip, deflate',
+//            ]
+//        ]);
+//
+//        $endPoint = env("MEEZA_WALLET_URL") . "/RequestToPay";
+//
+//        $response = $client->post($endPoint,
+//            ['body' => $data]
+//        );
+//
+//        $providerResponse = json_decode($response->getBody(), true);
+//        dd($providerResponse);
+//    }
+
+    /*=================================================================*/
+//    public function generateHash($date)
+//    {
+//        $key = env('MEEZA_Secure_Key');
+//        $MEEZA_Merchant_ID = env('MEEZA_Merchant_ID');
+//        $MEEZA_Terminal_ID = env('MEEZA_Terminal_ID');
+//        $secret = hex2bin($key);
+//        $string = "DateTimeLocalTrxn=$date&MerchantId={$MEEZA_Merchant_ID}&TerminalId={$MEEZA_Terminal_ID}";
+//        return hash_hmac('sha256', $string, $secret);
+//    }
     /*=================================================================*/
 }
--- resources/views/frontend/payment/mobile_wallet.blade.php
+++ resources/views/frontend/payment/mobile_wallet.blade.php
@@ -1,143 +1,38 @@
-<div class="modal fade mezza--modal" id="mobileWalletModalPayment" tabindex="-1" role="dialog"
-     aria-labelledby="mobileWalletModalPayment"
-     aria-hidden="true" data-backdrop="static">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLongTitle">Pay Using mobile wallet</h5>
-            </div>
-            <div class="modal-body">
-                <form id="mobileWalletForm" action="{{ route("mobileWallet.requestToPay") }}" method="post">
-                    @csrf
-                    <div class="row">
-                        <div class="form-group col-md-12">
-                            <input class="form-control cform" id="QRModel_R2PValue" maxlength="11" minlength="9"
-                                   name="mobileNumber" placeholder="Enter Mobile Number" type="text" value="">
-                        </div>
-                        <div class="form-group col-md-12">
-                            <button class="cantscanSend" type="button" id="sendPaymentRequest">submit</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-            </div>
-        </div>
-    </div>
-</div>
+<script>
+    if (document.querySelectorAll('meezaWalletSdk').length > 0) {
+        document.querySelectorAll('meezaWalletSdk').remove();
+    }
 
+    var script = document.createElement("script");
 
-<script src="{{ asset('assets/js/plugins/qrcode.min.js') }}"></script>
-<script type="application/javascript">
-    $(document).ready(function () {
-        $("#mobileWalletModalPayment").modal("show");
+    script.setAttribute("id", "meezaWalletSdk");
+    script.setAttribute("src", "{{ $meezaCardUrl }}");
+    document.getElementById('meezaCardDiv').appendChild(script);
 
-        /*================================================================*/
-        function sendPaymentRequest(event) {
-            event.preventDefault();
-            var request = $.ajax({
-                url: $("#mobileWalletForm").attr("action"),
-                dataType: "JSON",
-                type: "POST",
-                data: $("#mobileWalletForm").serialize()
-            });
+    script.onload = () => {
 
-            request.done((response) => {
+        var configObject = {
+            OrderId: "",
+            paymentMethodFromLightBox: 0,
+            MID: "{{ $merchantId }}",
+            TID: "{{ $terminalId }}",
+            AmountTrxn: "{{ $amount }}",
+            SecureHash: "{{ $secureHash }}",
+            TrxDateTime: "{{ $transactionDate }}",
+            completeCallback: function (data) {
+                var successUrl = "{{ $successUrl }}";
+                window.location.href = successUrl;
+            },
+            errorCallback: function (error) {
+                var cancelUrl = "{{ $cancelUrl }}";
+                window.location.href = cancelUrl;
+            },
+            cancelCallback: function () {
 
-            });
+            }
+        };
 
-        }
-
-        $(document).on("click", "#sendPaymentRequest", sendPaymentRequest);
-        /*================================================================*/
-        // $('#qrShowDiv').on('click', function () {
-        //     $(this).fadeToggle();
-        //     $('#qrcodeDiv').fadeToggle();
-        // });
-        //
-        // $('#mobileShowDiv').on('click', function () {
-        //     $(this).fadeToggle();
-        //     $('#mobilePayDiv').fadeToggle();
-        // });
-
-
-        // var refreshIntervalId = setInterval(checkStatus, 10000);
-
-        {{--var qrcodevar = new QRCode(document.getElementById("qrcode_img"), {--}}
-        {{--    text: "{{ $qrcode }}",--}}
-        {{--    width: 210,--}}
-        {{--    height: 210,--}}
-        {{--    colorDark: "#000000",--}}
-        {{--    colorLight: "#ffffff",--}}
-        {{--    correctLevel: QRCode.CorrectLevel.H--}}
-        {{--});--}}
-
-        // var time = setTimeout(function () {
-        //     $('#mobileWalletModalPayment').modal('show');
-        // }, 1000);
-
-        {{--function checkStatus() {--}}
-        {{--    if ($('#mobileWalletModalPayment').is(':visible')) {--}}
-        {{--        $.ajax({--}}
-        {{--            type: "POST",--}}
-        {{--            dataType: "JSON",--}}
-        {{--            url: '{{ route('meezaCheckStatus') }}',--}}
-        {{--            data: {--}}
-        {{--                '_token': '{{ csrf_token() }}',--}}
-        {{--                'transactionId': '{{ $transactionId }}',--}}
-        {{--                'referenceKey': '{{ $referenceKey }}',--}}
-        {{--                'vendorId': '{{ $vendorId }}',--}}
-        {{--                'transactionPrefix': '{{ $transactionPrefix }}'--}}
-        {{--            },--}}
-        {{--            success: function (response) {--}}
-        {{--                Swal.fire({--}}
-        {{--                    text: "Success Paid By Meeza",--}}
-        {{--                    icon: "success",--}}
-        {{--                    buttonsStyling: false,--}}
-        {{--                    confirmButtonText: "Ok",--}}
-        {{--                    customClass: {--}}
-        {{--                        confirmButton: "btn font-weight-bold btn-light"--}}
-        {{--                    }--}}
-        {{--                }).then((result) => {--}}
-        {{--                    window.location.href = response.redirectTo;--}}
-        {{--                });--}}
-        {{--            }--}}
-        {{--        });--}}
-        {{--    }--}}
-        {{--}--}}
-
-        // $(".cantscan").click(function () {
-        //     $(this).fadeOut(1000);
-        //     $(".MobileInput").fadeToggle("slow", "linear");
-        //     $("#QRModel_R2PValue").focus();
-        // });
-
-        {{--$(".cantscanSend").click(function (e) {--}}
-        {{--    e.preventDefault();--}}
-        {{--    var id = {{ $transactionId }};--}}
-        {{--    var mobileNumber = $('#QRModel_R2PValue').val();--}}
-        {{--    $.ajax({--}}
-        {{--        type: "POST",--}}
-        {{--        url: '{{ route('changeMeezaQR') }}',--}}
-        {{--        data: {--}}
-        {{--            '_token': '{{ csrf_token() }}',--}}
-        {{--            'transactionId': '{{ $transactionId }}',--}}
-        {{--            'referenceKey': '{{ $referenceKey }}',--}}
-        {{--            'vendorId': '{{ $vendorId }}',--}}
-        {{--            'transactionPrefix': '{{ $transactionPrefix }}',--}}
-        {{--            "mobileNumber": mobileNumber--}}
-        {{--        },--}}
-        {{--        success: function (data) {--}}
-        {{--            qrcodevar.clear();--}}
-        {{--            qrcodevar.makeCode(data);--}}
-
-        {{--            $('#QRModel_R2PValue').empty();--}}
-        {{--            $('#QRModel_R2PValue').val('');--}}
-        {{--            $('.MobileInput').fadeOut(1000);--}}
-        {{--            $(".cantscan").fadeIn(1000);--}}
-        {{--        }--}}
-        {{--    });--}}
-        {{--});--}}
-    });
+        Lightbox.Checkout.configure = configObject;
+        Lightbox.Checkout.showLightbox();
+    };
 </script>
